cf19a16e62a085f7682e42160eaa9135624c91e0
J-Phiz <jp.save@gmail.com>
2021-07-27 14:24:11 +0200

    Change on click method usage form each film

--- app/src/main/java/fr/jpsave/android/movieapp/MainActivity.java
+++ app/src/main/java/fr/jpsave/android/movieapp/MainActivity.java
@@ -16,7 +16,8 @@ import fr.jpsave.android.movieapp.constants.Constants;
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextViewWelcome;
-    private Button mButtonSearch;
+    private LinearLayout mLinearLayoutFilm1;
+    private LinearLayout mLinearLayoutFilm2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,15 +26,34 @@ public class MainActivity extends AppCompatActivity {
 
         Log.d("ChezMoi Processus", "MainActivity: onCreate()");
 
-        //Toast.makeText(this, "BlaBlaBla... Je ne dis jamais BlaBlaBla!!!", Toast.LENGTH_SHORT).show();
         mTextViewWelcome = (TextView) findViewById(R.id.text_view_welcome);
         mTextViewWelcome.setText(getString(R.string.welcome, "JP"));
         Toast.makeText(this, mTextViewWelcome.getText(), Toast.LENGTH_SHORT).show();
 
-        mButtonSearch = (Button) findViewById(R.id.button_search);
-        mButtonSearch.setOnClickListener(
-            view -> Log.d("ChezMoi", "Click sur bouton rechercher")
+        mLinearLayoutFilm1 = (LinearLayout) findViewById(R.id.linear_layout_film1);
+        mLinearLayoutFilm1.setOnClickListener(
+            view -> this.onClickFilm(
+                ((TextView) view.findViewById(R.id.text_view_film1)).getText().toString()
+            )
         );
+
+        mLinearLayoutFilm2 = (LinearLayout) findViewById(R.id.linear_layout_film2);
+        mLinearLayoutFilm2.setOnClickListener(
+            view -> this.onClickFilm(
+                ((TextView) view.findViewById(R.id.text_view_film2)).getText().toString()
+            )
+        );
+
+    }
+
+    public void defaultOnClickSearchButton(View view) {
+        Log.d("ChezMoi", "Click sur bouton rechercher avec methode par defaut");
+    }
+
+    public void onClickFilm(String filmTitle) {
+        Intent intent = new Intent(this, MovieActivity.class);
+        intent.putExtra(Constants.MOVIE_TITLE_KEY, filmTitle);
+        startActivity(intent);
     }
 
     @Override
@@ -70,28 +90,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         Log.d("ChezMoi Processus", "MainActivity: onStop()");
-    }
-
-    public void defaultOnClickSearchButton(View view) {
-        Log.d("ChezMoi", "Click sur bouton rechercher avec methode par defaut");
-    }
-
-    public void onClickFilm(View view) {
-        LinearLayout linearLayout = (LinearLayout) view;
-        String message = "Click on Film ";
-        String filmTitle = "";
-
-        if (linearLayout.getId() == R.id.linear_layout_film1) {
-            filmTitle = ((TextView) view.findViewById(R.id.text_view_film1)).getText().toString();
-        } else if (linearLayout.getId() == R.id.linear_layout_film2) {
-            filmTitle = ((TextView) view.findViewById(R.id.text_view_film2)).getText().toString();
-        }
-
-        Intent intent = new Intent(this, MovieActivity.class);
-        intent.putExtra(Constants.MOVIE_TITLE_KEY, filmTitle);
-        startActivity(intent);
-
-        Toast.makeText(this, message + filmTitle, Toast.LENGTH_SHORT).show();
     }
 
 }
