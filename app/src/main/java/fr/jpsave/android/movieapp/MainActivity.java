3f493ed2e7272889cde10bc4cc04981cc8e5886a
J-Phiz <jp.save@gmail.com>
2021-07-27 11:07:58 +0200

    Add a toast for click on films

--- app/src/main/java/fr/jpsave/android/movieapp/MainActivity.java
+++ app/src/main/java/fr/jpsave/android/movieapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,14 +25,26 @@ public class MainActivity extends AppCompatActivity {
         mTextViewWelcome.setText(getString(R.string.welcome, "JP"));
         Toast.makeText(this, mTextViewWelcome.getText(), Toast.LENGTH_SHORT).show();
 
-//        mButtonSearch = (Button) findViewById(R.id.button_search);
-//        mButtonSearch.setOnClickListener(
-//            view -> Log.d("ChezMoi", "Click sur bouton rechercher")
-//        );
+        mButtonSearch = (Button) findViewById(R.id.button_search);
+        mButtonSearch.setOnClickListener(
+            view -> Log.d("ChezMoi", "Click sur bouton rechercher")
+        );
     }
 
     public void defaultOnClickSearchButton(View view) {
         Log.d("ChezMoi", "Click sur bouton rechercher avec methode par defaut");
+    }
+
+    public void onClickFilm(View view) {
+        LinearLayout linearLayout = (LinearLayout) view;
+        String message = "Click on Film ";
+
+        if (linearLayout.getId() == R.id.linear_layout_film1)
+            message += ((TextView) view.findViewById(R.id.text_view_film1)).getText().toString();
+        if (linearLayout.getId() == R.id.linear_layout_film2)
+            message += ((TextView) view.findViewById(R.id.text_view_film2)).getText().toString();
+
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 
 }
