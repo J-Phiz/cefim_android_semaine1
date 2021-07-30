f67fd5fa22fca02de9c5b2ed8c4d5be89add5310
J-Phiz <jp.save@gmail.com>
2021-07-30 10:56:27 +0200

    Add a progress bar and network check for search view

--- app/src/main/java/fr/jpsave/android/movieapp/activity/SearchActivity.java
+++ app/src/main/java/fr/jpsave/android/movieapp/activity/SearchActivity.java
@@ -12,6 +12,8 @@ import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.SearchView;
 import android.widget.TextView;
 
@@ -41,7 +43,8 @@ public class SearchActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private SearchAdapter mSearchAdapter;
     private OkHttpClient mOkHttpClient;
-    private String searchText;
+    private ProgressBar mPbloading;
+    private LinearLayout mLlAllContent;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -54,10 +57,15 @@ public class SearchActivity extends AppCompatActivity {
         mMovies = new ArrayList<>();
         mSvSearch = findViewById(R.id.search_view);
         mRecyclerView = findViewById(R.id.recycler_view);
+        mPbloading = findViewById(R.id.progress_bar_load_list);
+        mLlAllContent =  findViewById(R.id.linear_layout_movies_list);
 
+        // Manage Search View
         mSvSearch.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String s) {
+                mPbloading.setVisibility(View.VISIBLE);
+                mPbloading.animate();
                 // Call API
                 String url = getString(R.string.movie_base_url) + "&s=" + s.replace(" ", "%20");
                 Request request = new Request.Builder().url(url).build();
@@ -65,17 +73,25 @@ public class SearchActivity extends AppCompatActivity {
                     @Override
                     public void onFailure(Call call, IOException e) {
                         Log.d("ChezMoi", "Movie API Communication failure");
+                        ((TextView) findViewById(R.id.text_view_no_internet)).setVisibility(View.VISIBLE);
+                        mPbloading.setVisibility(View.INVISIBLE);
                     }
 
                     @Override
                     public void onResponse(Call call, Response response) throws IOException {
                         final String stringJson = response.body().string();
                         Log.d("ChezMoi", "Movie API Communication OK\n" + stringJson);
-
+                        // Attendre juste pour voir le progressBar
+                        try {
+                            Thread.sleep(2000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
                                 // Code exécuté dans le Thread principale
+                                mPbloading.setVisibility(View.GONE);
                                 Gson gson = new Gson();
                                 Search search = (gson.fromJson(stringJson, Search.class));
                                 mMovies.removeAll(mMovies);
@@ -96,8 +112,22 @@ public class SearchActivity extends AppCompatActivity {
             }
         });
 
+        // Manage Recycler View
         mRecyclerView.setLayoutManager(new GridLayoutManager(this, 3));
         mSearchAdapter = new SearchAdapter(mContext, mMovies);
         mRecyclerView.setAdapter(mSearchAdapter);
+
+        // Manage Internet Access
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            // Oui il y a Internet je lance un appel API
+            mLlAllContent.setVisibility(View.VISIBLE);
+        } else {
+            // Non... J’affiche un message à l’utilisateur
+            ((TextView) findViewById(R.id.text_view_no_internet)).setVisibility(View.VISIBLE);
+            mLlAllContent.setVisibility(View.INVISIBLE);
+            mPbloading.setVisibility(View.GONE);
+        }
     }
 }
